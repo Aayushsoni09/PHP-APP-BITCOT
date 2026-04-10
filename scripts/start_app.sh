fe51f0e425d7bdb771746901518a0e88998a4d4e
Aayush Soni <44709621+Aayushsoni09@users.noreply.github.com>
2026-04-10 13:59:43 +0530

    Remove hardcoded DB credentials from start_app.sh
    Removed sensitive database credentials from Docker run command.

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -17,10 +17,10 @@ docker pull 381491835701.dkr.ecr.ap-south-1.amazonaws.com/bitcot-registry:latest
 
 echo "Starting Docker container..."
 docker run -d -p 80:80 \
-  -e DB_HOST=bitcot-db.cbaa6kqecpeh.ap-south-1.rds.amazonaws.com \
-  -e DB_USER=admin \
-  -e DB_PASS=Admin1234 \
-  -e DB_NAME=php_app_db \
+  -e DB_HOST="" \
+  -e DB_USER="" \
+  -e DB_PASS="" \
+  -e DB_NAME="" \
   381491835701.dkr.ecr.ap-south-1.amazonaws.com/bitcot-registry:latest
 
 echo "Application started successfully."
